21e58ea7dfc2e8afbbe7ea8793543d4d6a878e87
vstrom60xt <cerveramonzo@gmail.com>
2024-02-19 16:45:09 +0100

    antes de iniciosesi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cloud-kitchen';
+  title = 'Pec-Bar';
 }
---
+++ src/app/iniciosesion/iniciosesion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciosesion',
+  standalone: true,
+  imports: [],
+  templateUrl: './iniciosesion.component.html',
+  styleUrl: './iniciosesion.component.css'
+})
+export class IniciosesionComponent {
+
+}
