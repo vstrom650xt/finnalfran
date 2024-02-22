dc8195afb17b200fe2a160d32481541ac26cfd40
vstrom650xt <cerveramonzo@gmail.com>
2024-02-22 17:05:50 +0100

    qqq

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cloud-kitchen'),
+      dir: require('path').join(__dirname, './coverage/projects.franfinalangular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cloud-kitchen'`, () => {
+  it(`should have as title 'projects.franfinalangular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cloud-kitchen');
+    expect(app.title).toEqual('projects.franfinalangular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cloud-kitchen app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projects.franfinalangular app is running!');
   });
 });
